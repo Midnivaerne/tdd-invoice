e471a2e88c6692e006593a9a27d41056ee2c6dce
student23 <student23@s10n-mwo-27.pcoip.ki.agh.edu.pl>
2019-02-09 12:04:06 +0100

    Added implementation for all Product and Invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,43 +1,48 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Collection<Product> products = new ArrayList<Product>();
 
 	public void addProduct(Product product) {
 		this.products.add(product);
+		System.out.println(product.getName() + " " + product.getPrice() + " " + product.getTaxPercent());
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		for(int i=0; i < quantity; i++ )
+		if (quantity <= 0) {
+			throw new IllegalArgumentException("Invalid quantity");
+		}
+		for (int i = 0; i < quantity; i++) {
 			this.products.add(product);
+		}
 	}
 
 	public BigDecimal getSubtotal() {
 		BigDecimal subtotal = BigDecimal.ZERO;
-		for(Product product : this.products) {
-			subtotal.add(product.getPrice());
+		for (Product product : this.products) {
+			subtotal = subtotal.add(product.getPrice());
 		}
 		return subtotal;
 	}
 
 	public BigDecimal getTax() {
 		BigDecimal tax = BigDecimal.ZERO;
-		for(Product product : products) {
-			tax.add(product.getPriceWithTax().subtract(product.getPrice()));
+		for (Product product : products) {
+			tax = tax.add(product.getPriceWithTax().subtract(product.getPrice()));
 		}
 		return tax;
 	}
 
 	public BigDecimal getTotal() {
 		BigDecimal total = BigDecimal.ZERO;
-		for(Product product : products) {
-			total.add(product.getPriceWithTax());
+		for (Product product : products) {
+			total = total.add(product.getPriceWithTax());
 		}
 		return total;
 	}
